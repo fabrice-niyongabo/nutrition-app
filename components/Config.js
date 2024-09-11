987047f6650b4fa9ff345db09cf287e3fdd89c0f
Fabrice Niyongabo <ninga.fab@gmail.com>
2024-09-11 08:39:03 +0200

    icons

--- components/Config.js
+++ components/Config.js
@@ -1,9 +1,9 @@
-module.exports = {
-  backendUrl: 'http://192.168.11.3/RN/NMIS/',
-  imageUrl: 'http://192.168.11.3/RN/NMIS/img/',
-};
+// module.exports = {
+//   backendUrl: 'http://192.168.11.3/RN/NMIS/',
+//   imageUrl: 'http://192.168.11.3/RN/NMIS/img/',
+// };
 
-//module.exports = {
-// backendUrl: 'https://api.sineducationltd.com/NIS/',
-//imageUrl: 'https://api.sineducationltd.com/NIS/img/',
-//};
+module.exports = {
+  backendUrl: 'https://nis.cyizere.rw/',
+  imageUrl: 'https://nis.cyizere.rw/img/',
+};
