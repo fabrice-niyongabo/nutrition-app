800a109e3a71b5d1a2e72c28d199797d0078afd3
Fabrice Niyongabo <ninga.fab@gmail.com>
2025-01-08 20:43:35 +0200

    prod

--- components/Config.js
+++ components/Config.js
@@ -1,9 +1,9 @@
-module.exports = {
-  backendUrl: 'http://192.168.210.3:4000/api',
-  imageUrl: 'http://192.168.210.3:4000/uploads/',
-};
-
 // module.exports = {
-//   backendUrl: 'https://nutritions-mis.onrender.com/api',
-//   imageUrl: 'https://nutritions-mis.onrender.com/uploads/',
+//   backendUrl: 'http://192.168.210.3:4000/api',
+//   imageUrl: 'http://192.168.210.3:4000/uploads/',
 // };
+
+module.exports = {
+  backendUrl: 'https://nutritions-mis.onrender.com/api',
+  imageUrl: 'https://nutritions-mis.onrender.com/uploads/',
+};
