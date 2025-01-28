9857d18fdb6178bcb3bc0a00c0777a9fc80b67f4
Fabrice Niyongabo <ninga.fab@gmail.com>
2025-01-28 13:11:50 +0200

    auth fix

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,4 +1,5 @@
 import {Notifier, NotifierComponents} from 'react-native-notifier';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const toastMessage = (
   type: 'error' | 'success' | 'warn' | 'info',
@@ -15,4 +16,21 @@ export const toastMessage = (
       alertType: type,
     },
   });
+};
+
+export const errorHandler = (error: any) => {
+  if (error?.response?.data?.message) {
+    toastMessage('error', error.response.data.message);
+  } else if (error.message) {
+    toastMessage('error', error.message);
+  } else {
+    toastMessage('error', error);
+  }
+  handleAuthError(error);
+};
+
+export const handleAuthError = (error: any) => {
+  if (error?.response?.status == 401) {
+    AsyncStorage.clear();
+  }
 };
