a39d105664868b9ef33775a81dd7c49a38fe7482
Fabrice Niyongabo <ninga.fab@gmail.com>
2025-02-01 16:06:03 +0200

    sending auth token

--- src/screens/afterLogin/womanMealPrediction/index.tsx
+++ src/screens/afterLogin/womanMealPrediction/index.tsx
@@ -7,11 +7,14 @@ import {IWoman} from '../../../types/women';
 import {errorHandler} from '../../../utils/helpers';
 import {COLORS} from '../../../constants/colors';
 import Graph from './Graph';
+import {useSelector} from 'react-redux';
+import {RootState} from '../../../redux/reducers';
 
 const WomanMealPrediction = ({
   route,
   navigation,
 }: INavigationPropWithRouteRequired) => {
+  const {token} = useSelector((state: RootState) => state.userReducer);
   const {woman} = route.params as {woman: IWoman};
   const [isLoading, setIsLoading] = useState(false);
   const [mealsData, setMealsData] = useState<IMealItem[]>([]);
@@ -21,6 +24,11 @@ const WomanMealPrediction = ({
       setIsLoading(true);
       const res = await axios.get(
         APP.backendUrl + '/women/mealhistory/' + woman.id,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
       );
       setMealsData(res.data.meals);
     } catch (error) {
